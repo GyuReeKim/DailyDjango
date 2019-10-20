49b78fbdd8681633b330923f5355e9d4a2fa7837
GyuReeKim <starkim96@naver.com>
2019-10-20 11:36:53 +0900

    1020

--- Oct1018/accounts/views.py
+++ Oct1018/accounts/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect('todos:index')
+        
     if request.method == "POST":
-        pass
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
@@ -15,4 +19,24 @@ def signup(request):
     context = {
         'form': form,
     }
-    return render(request, 'accounts/signup.html', context)
+    return render(request, 'accounts/form.html', context)
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('todos:index')
+    
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get('next') or 'todos:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/form.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('todos:index')
--- Oct1018/todos/views.py
+++ Oct1018/todos/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import TodoForm
+from django.contrib.auth.decorators import login_required
+from .models import Todo
 
 # Create your views here.
+@login_required
 def index(request):
-    return render(request, 'todos/index.html')
+    todos = request.user.todo_set.all().order_by('due_date')
+    context = {
+        'todos': todos,
+    }
+    return render(request, 'todos/index.html', context)
+
+@login_required
+def create(request):
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.user = request.user
+            todo.save()
+            return redirect('todos:index')
+    else:
+        form = TodoForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'todos/form.html', context)
+
+@login_required
+def delete(request, id):
+    todo = get_object_or_404(Todo, id=id)
+    if todo.user == request.user:
+        todo.delete()
+    return redirect('todos:index')

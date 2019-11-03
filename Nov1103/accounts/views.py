d128c7ca417b77affcb0821e799c4c80223ec65f
GyuReeKim <starkim96@naver.com>
2019-11-03 12:44:08 +0900

    기본 account 생성

--- Nov1103/accounts/views.py
+++ Nov1103/accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/form.html', {'form': form})
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('accounts:signup')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/form.html', {'form': form})
+
+def logout(request):
+    auth_logout(request)
+    return redirect('accounts:signup')

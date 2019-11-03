e20620c47212ef5da554723985a0be509bb24c22
GyuReeKim <starkim96@naver.com>
2019-11-03 22:31:33 +0900

    팔로우 & 회원탈퇴 & 회원정보 수정 & 비밀번호 변경

--- Nov1103/accounts/urls.py
+++ Nov1103/accounts/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('signup/', views.signup, name="signup"),
     path('login/', views.login, name="login"),
     path('logout/', views.logout, name="logout"),
-    path('<int:id>/', views.user_page, name="user_page"),
+    path('users/', views.user_list, name="user_list"),
+    path('<int:user_id>/', views.user_page, name="user_page"),
+    path('<int:user_id>/follow/', views.follow, name="follow"),
+    path('<int:user_id>/delete/', views.delete, name="delete"),
+    path('<int:user_id>/update/', views.update, name="update"),
+    path('<int:user_id>/password/', views.password, name="password"),
 ]
--- Nov1103/accounts/views.py
+++ Nov1103/accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-from .forms import CustomUserCreationForm
+from django.contrib.auth import update_session_auth_hash
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import User
 
 # Create your views here.
@@ -30,7 +31,50 @@ def logout(request):
     auth_logout(request)
     return redirect('posts:index')
 
-def user_page(request, id):
-    user_info = get_object_or_404(User, id=id)
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'accounts/user_list.html', {'users': users})
+
+def user_page(request, user_id):
+    user_info = get_object_or_404(User, id=user_id)
     return render(request, 'accounts/user_page.html', {'user_info': user_info})
+
+def follow(request, user_id):
+    you = get_object_or_404(User, id=user_id)
+    me = request.user
     
+    if you != me:
+        if me in you.followers.all():
+            you.followers.remove(me)
+        else:
+            you.followers.add(me)
+    return redirect('accounts:user_page', user_id)
+
+def delete(request, user_id):
+    user_info = get_object_or_404(User, id=user_id)
+    user = request.user
+
+    if user == user_info:
+        user.delete()
+    return redirect('posts:index')
+
+def update(request, user_id):
+    if request.method == "POST":
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:user_page', user_id)
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    return render(request, 'accounts/form.html', {'form': form})
+
+def password(request, user_id):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('accounts:user_page', user_id)
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'accounts/form.html', {'form': form})
